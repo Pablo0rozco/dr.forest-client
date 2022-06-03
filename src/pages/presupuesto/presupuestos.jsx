e145978d9110ec1de82a9a6686814e73da3b799a
Josu Martin <josu.martin.sanchez@gmail.com>
2022-06-03 16:33:14 +0200

    doing

--- src/pages/presupuesto/presupuestos.jsx
+++ src/pages/presupuesto/presupuestos.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 import { useNavigate } from "react-router-dom";
-import { crearPresupuestoService } from "../../services/Presupuestos.services.js";
+import { crearPresupuestoService } from "../../services/presupuestos.services.js";
 
 function Presupuestos() {
   const navigate = useNavigate();
