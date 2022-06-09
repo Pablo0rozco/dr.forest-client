550ffbda000fe2cd338718693d9d8d7025e6b390
Josu Martin <josu.martin.sanchez@gmail.com>
2022-06-09 15:13:45 +0200

    navBar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,9 @@ function Navbar() {
   };
 
   const activeStyles = {
-    textDecoration: "underline",
+    width: " 18%",
+    height: "55%",
+    fontSize: "30px",
   };
 
   const inActiveStyles = {
@@ -31,146 +33,83 @@ function Navbar() {
   return (
     <div>
       {/* // 1º Si Logeado y Profesional */}
-      <div>
-        {isLoggedIn === true && isProfesional === true && (
+      {isLoggedIn === true && isProfesional === true && (
+        <div className="navbar-general">
+          <nav className="navbar-conainer">
+            <NavLink className="NavLink" to="/" style={toggleStyles}>
+              Home
+            </NavLink>
 
-          
-          <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
-            <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-              <div className="text-sm lg:flex-grow">
-                <a
-                  href="#responsive-header"
-                  className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
-                >
-                  <NavLink to="/" style={toggleStyles}>
-                    {" "}
-                    Home{" "}
-                  </NavLink>
-                </a>
-                <a
-                  href="#responsive-header"
-                  className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
-                >
-                  <NavLink to="/presupuestos/empresa" style={toggleStyles}>
-                    {" "}
-                    Ver Solicitudes{" "}
-                  </NavLink>
-                </a>
-                <a
-                  href="#responsive-header"
-                  className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white"
-                >
-                  <NavLink to="/servicios/crear" style={toggleStyles}>
-                    {" "}
-                    Crear servicio{" "}
-                  </NavLink>
-                </a>
+            <NavLink
+              className="NavLink"
+              to="/presupuestos/empresa"
+              style={toggleStyles}
+            >
+              Ver Solicitudes
+            </NavLink>
 
-                <a
-                  href="#responsive-header"
-                  className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white"
-                >
-                  <NavLink to="/perfil" style={toggleStyles}>
-                    Ver Perfil
-                  </NavLink>
-                </a>
+            <NavLink
+              className="NavLink"
+              to="/servicios/crear"
+              style={toggleStyles}
+            >
+              Crear servicio
+            </NavLink>
 
-                <a
-                  href="#responsive-header"
-                  className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white"
-                >
-                  <button onClick={handleLogout}>Cerrar Sesion</button>
-                </a>
-              </div>
-            </div>
+            <NavLink className="NavLink" to="/perfil" style={toggleStyles}>
+              Ver Perfil
+            </NavLink>
+
+            <button onClick={handleLogout}>Cerrar Sesion</button>
           </nav>
-        )}
-      </div>
+        </div>
+      )}
 
       {/* // 2º Si logeado y cliente */}
       <div>
         {isLoggedIn === true && isCliente === true && (
-          <nav className="flex items-center justify-between flex-wrap bg-teal-500 p-6">
-            <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-              <div className="text-sm lg:flex-grow">
-                <a
-                  href="#responsive-header"
-                  className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
-                >
-                  <NavLink to="/" style={toggleStyles}>
-                    {" "}
-                    Home{" "}
-                  </NavLink>
-                </a>
+          <div className="navbar-general">
+            <nav className="navbar-conainer">
+              <NavLink className="NavLink" to="/" style={toggleStyles}>
+                Home
+              </NavLink>
 
-                <a
-                  href="#responsive-header"
-                  className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
-                >
-                  <NavLink to="/presupuestos" style={toggleStyles}>
-                    {" "}
-                    Ver tus presupuestos{" "}
-                  </NavLink>
-                </a>
+              <NavLink
+                className="NavLink"
+                to="/presupuestos"
+                style={toggleStyles}
+              >
+                Ver Presupuestos
+              </NavLink>
 
-                <a
-                  href="#responsive-header"
-                  className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white"
-                >
-                  <NavLink to="/perfil" style={toggleStyles}>
-                    Ver Perfil
-                  </NavLink>
-                </a>
+              <NavLink className="NavLink" to="/perfil" style={toggleStyles}>
+                Ver Perfil
+              </NavLink>
 
-                <a
-                  href="#responsive-header"
-                  className="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white"
-                >
-                  <button onClick={handleLogout}>Cerrar Sesion</button>
-                </a>
-              </div>
-            </div>
-          </nav>
+              <button onClick={handleLogout}>Cerrar Sesion</button>
+            </nav>
+          </div>
         )}
       </div>
 
       {/* // 3º Si no logeado */}
       <div>
         {isLoggedIn === false && (
-          <nav class="flex items-center justify-between flex-wrap bg-[#15803d] p-6">
-            <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-              <div class="text-sm lg:flex-grow">
-                <a
-                  href="#responsive-header"
-                  class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
-                >
-                  <NavLink to="/" style={toggleStyles}>
-                    {" "}
-                    Home{" "}
-                  </NavLink>
-                </a>
+          <div className="navbar-general">
+            <nav className="navbar-conainer">
+              <NavLink className="NavLink" to="/" style={toggleStyles}>
+                Home
+              </NavLink>
 
-                <a
-                  href="#responsive-header"
-                  class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4"
-                >
-                  <NavLink to="/signup" style={toggleStyles}>
-                    {" "}
-                    Registrase{" "}
-                  </NavLink>
-                </a>
-                <a
-                  href="#responsive-header"
-                  class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white"
-                >
-                  <NavLink to="/login" style={toggleStyles}>
-                    {" "}
-                    Acceder{" "}
-                  </NavLink>
-                </a>
-              </div>
-            </div>
-          </nav>
+              <NavLink className="NavLink" to="/signup" style={toggleStyles}>
+                Registrase
+              </NavLink>
+
+              <NavLink className="NavLink" to="/login" style={toggleStyles}>
+                Acceder
+              </NavLink>
+            </nav>
+          </div>
         )}
       </div>
     </div>
